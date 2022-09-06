cafdb1ddebf77753efe5fc7658616a15b343647a
BuildTools <unconfigured@null.spigotmc.org>
2022-09-06 19:46:41 +0300

    fix

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.post("/convert-mp3", async (req, res) => {
         return res.render("index", {success: false, message : fetchResponse.msg})
     }
 })
-//server start
-app.listen(PORT, () => {
-    console.log(`server started on port ${PORT}`);
-})
+//start server
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
